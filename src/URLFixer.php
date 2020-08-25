8da9f7e790ace74d34c969edfacb96e12f2a21e5
Philipp <32090713+ouun@users.noreply.github.com>
2020-08-25 12:40:16 +0200

    Update URLFixer.php
    Allow setting WP_INSTALL_DIR with fallback to 'wp'

--- src/URLFixer.php
+++ src/URLFixer.php
@@ -11,6 +11,19 @@ namespace Roots\Bedrock;
 class URLFixer
 {
     /**
+     * Get WordPress Install Dir
+     *
+     * @param string $prepend
+     * @param string $append
+     *
+     * @return string
+     */
+    public function wpInstallDir($prepend = '', $append = '')
+    {
+        return $prepend . defined(WP_INSTALL_DIR) ? WP_INSTALL_DIR : 'wp' . $append;
+    }
+
+    /**
      * Add filters to verify / fix URLs.
      */
     public function addFilters()
@@ -28,8 +41,10 @@ class URLFixer
      */
     public function fixHomeURL($value)
     {
-        if (substr($value, -10) === '/backstage') {
-            $value = substr($value, 0, -10);
+        $wp_path = self::wpInstallDir('/');
+
+        if (substr($value, -strlen($wp_path)) === $wp_path) {
+            $value = substr($value, 0, -3);
         }
         return $value;
     }
@@ -42,8 +57,10 @@ class URLFixer
      */
     public function fixSiteURL($url)
     {
-        if (substr($url, -10) !== '/backstage' && (is_main_site() || is_subdomain_install())) {
-            $url .= '/backstage';
+        $wp_path = self::wpInstallDir('/');
+
+        if (substr($url, -strlen($wp_path)) !== $wp_path && (is_main_site() || is_subdomain_install())) {
+            $url .= $wp_path;
         }
         return $url;
     }
@@ -60,9 +77,11 @@ class URLFixer
     {
         $path = ltrim($path, '/');
         $url = substr($url, 0, strlen($url) - strlen($path));
+        $wp_path = self::wpInstallDir('', '/');
 
-        if (substr($url, -10) !== 'backstage/') {
-            $url .= 'backstage/';
+
+        if (substr($url, -strlen($wp_path)) !== $wp_path) {
+            $url .= $wp_path;
         }
 
         return $url . $path;
